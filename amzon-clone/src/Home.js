1be1d623dc5e6ededdecf0771979160315c85cac
nmemarcoding <nmemarcoding@gmail.com>
2022-03-31 10:24:23 -0700

    home created

---
+++ amzon-clone/src/Home.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import'./Home.css'
+function Home() {
+    return (
+        <div className="home">
+            <div className="home__container">
+                <img className="home__Image" src="https://m.media-amazon.com/images/I/611w8CPT7GL._SX3000_.jpg" alt="" />
+            </div>
+            <div className="home__row">
+
+            </div>
+            <div className="home__row">
+                
+            </div>
+            <div className="home__row">
+                
+            </div>
+        </div>
+        
+    )
+}
+
+export default Home
