3ee9b5b61c29fbc8c4e484e3801841ea34bea410
nmemarcoding <nmemarcoding@gmail.com>
2022-03-31 11:00:04 -0700

    product container created

--- amzon-clone/src/Home.js
+++ amzon-clone/src/Home.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import'./Home.css'
+import Product from "./Product.js"
 function Home() {
     return (
         <div className="home">
@@ -7,7 +8,7 @@ function Home() {
                 <img className="home__Image" src="https://m.media-amazon.com/images/I/611w8CPT7GL._SX3000_.jpg" alt="" />
             </div>
             <div className="home__row">
-
+                <Product/>
             </div>
             <div className="home__row">
                 
--- amzon-clone/src/Product.js
+++ amzon-clone/src/Product.js
@@ -3,6 +3,19 @@ import './Product.css'
 
 export default function Product() {
   return (
-    <div>Product</div>
+    <div className="product">
+        <div className="product__info">
+            <p>The Lean Startup</p>
+            <p className="product__price">
+                <small>$</small>
+                <strong>19.99</strong>
+            </p>
+        </div>
+        <div className="product__rating">
+            <p>⭐️⭐️⭐️</p>
+        </div>
+        <img src="https://m.media-amazon.com/images/I/81SrwYY-6-L._AC_UL640_FMwebp_QL65_.jpg" alt="" />
+        <button>Add to basket</button>
+    </div>
   )
 }
